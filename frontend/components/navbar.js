f0498c6c91a75745e4289e0b3dca5d9090db6021
shivam5665 <shivamsharmaji04@gmail.com>
2023-01-19 00:03:55 +0530

    push

---
+++ frontend/components/navbar.js
@@ -0,0 +1,30 @@
+
+const navbar = function(){
+    return `<div id="navbar">
+    <div id="logo" >
+     <img src="./imgs/list.png" style = "width: 30px" style ="height: 20px;" alt="">
+     <img src="./imgs/light (2).png" style = "width: 100%" alt="">
+    </div>
+    <div id="search">
+     <select name="" id="cate">
+         <option value="">ALL Category</option>
+     </select>
+     <input type="text" placeholder="seacrh" id="search_item">
+     <button><img src="./imgs/seach.png" alt="" style="width: 75%;"></button>
+    </div>
+    <div id="user">
+     <div>
+         <img src="./imgs/add-user-3-32.png" alt="">
+         <a href="" >
+             <p>Login/</p>
+         </a>
+         <a href=""><p>Register</p></a></div>
+         <div>
+             <a href=""><img src="./imgs/cart-41-32.png" style="width:100%" alt=""><p>Cart</p></a>
+         </div>
+     </div>
+     
+     
+
+ </div>`
+}
